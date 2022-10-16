fb92a4d26db4cb2393ed801981e20444a7ec944e
Akshay Khale <akshay.k@peopleinteractive.in>
2022-10-16 22:38:57 +0530

    [CodeOptimization] Create common component for profile to avoid code repeatation

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Profile from '../components/Profile'
 
 export default function About() {
   return (
@@ -11,6 +12,7 @@ export default function About() {
       </Head>
       <main className="mx-auto flex flex-col items-center justify-center my-10">
         <h1 className="my-10 font-semibold text-4xl">It&apos;s a simple Card Generator</h1>
+        <Profile />
       </main>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React from 'react'
+import Profile from '../components/Profile'
 
 const fetchProfileDetails = async () => {
   const response = await fetch('https://api.stackexchange.com/2.3/users/2541634?order=desc&sort=reputation&site=stackoverflow');
@@ -29,39 +30,7 @@ export default function Home({ profileDetails }) {
 
       <main className="mx-auto flex flex-col items-center justify-center my-10">
         <h1 className="my-10 font-semibold text-4xl">Stackoverflow Card generator</h1>
-        <section className="bg-gray-200 p-5 w-60 rounded-md text-center">
-          <a className="text-xs mt-1 text-gray-600 underline" target="_blank" rel="noreferrer" href={profileDetails.link}>
-            <img className="w-7" src="./stack-overflow.png" alt="StackOverflow" />
-          </a>
-          <img src={profileDetails.profile_image} alt="User Profile" className="w-28 mx-auto rounded-full" />
-          <div className="mt-4 font-bold text-xl">{profileDetails.display_name}</div>
-          <a className="text-xs mt-1 text-gray-600 underline" target="_blank" rel="noreferrer" href={profileDetails.website_url}>
-            {profileDetails.website_url}
-          </a>
-          <div className="w-28 h-[1px] mt-2 bg-gray-300 mx-auto"></div>
-          <div>
-            <div className="text-3xl mt-2 text-yellow-600">{profileDetails.reputation}</div>
-            <div className="text-xs mt-1 text-gray-600">StackOverflow Reputation</div>
-          </div>
-          <div className="w-28 mt-2 h-[1px] bg-gray-300 mx-auto"></div>
-          <div>
-            <div className="text-xs mt-2 text-gray-600">Badges</div>
-            <div className="flex justify-center gap-5 mt-3">
-              <div>
-                <div className="w-3 h-3 mx-auto rounded-full bg-yellow-500 mb-1" />
-                <div>{profileDetails.badge_counts.gold}</div>
-              </div>
-              <div>
-                <div className="w-3 h-3 mx-auto rounded-full bg-gray-500 mb-1" />
-                <div>{profileDetails.badge_counts.silver}</div>
-              </div>
-              <div>
-                <div className="w-3 h-3 mx-auto rounded-full bg-yellow-700 mb-1" />
-                <div>{profileDetails.badge_counts.bronze}</div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Profile profileDetails={profileDetails} />
         <section className="my-10">
           <div>
             <h1 className="mb-3 font-semibold text-xl text-center">Generate your&apos;s here:</h1>
